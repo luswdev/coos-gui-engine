8099f6bf1d3a3cfe0412645ac131f6351ce54651
omuskywalker <610721230@gms.ndhu.edu.tw>
2019-10-04 16:39:48 +0800

    add color's file comment for doxygen

--- include/color.h
+++ include/color.h
@@ -10,8 +10,9 @@
 #ifndef _COGUI_COLOR_H
 #define _COGUI_COLOR_H
 
-typedef co_int64_t cogui_color_t;
+typedef co_int64_t cogui_color_t;   /**< color type belong to long integer */
 
+/** determine which color style used */
 #define COGUI_USING_RGB565
 //#define COGUI_USING_BGR565
 //#define COGUI_USING_RGB888
@@ -19,6 +20,7 @@ typedef co_int64_t cogui_color_t;
 //#define COGUI_USING_RGBA8888
 //#define COGUI_USING_ABGR8888
 
+/* rgb into integer by different style */
 #define COGUI_RGB565(r,g,b)     ((cogui_color_t)((((r)>>3)<<11)|(((g)>>2)<<5)|((b)>>3)))
 #define COGUI_BGR565(b,g,r)     ((cogui_color_t)((((b)>>3)<<11)|(((g)>>2)<<5)|((r)>>3)))
 #define COGUI_RGB888(r,g,b)     ((cogui_color_t)(((r)<<16)|((g)<<8)|(b)))
@@ -50,17 +52,17 @@ typedef co_int64_t cogui_color_t;
 #define COGUI_RGB(r,g,b) COGUI_ABGR8888(255,(b),(g),(r))
 #endif
 
-
-#define COGUI_RED               COGUI_RGB(0xc6, 0x28, 0x28)
-#define COGUI_GREEN             COGUI_RGB(0x38, 0x8e, 0x3c)
-#define COGUI_BLUE              COGUI_RGB(0x02, 0x77, 0xbd)
-#define COGUI_YELLOW            COGUI_RGB(0xfb, 0xc0, 0x2d)
-#define COGUI_PURPLE            COGUI_RGB(0x51, 0x35, 0xb1)
-#define COGUI_CYAN              COGUI_RGB(0x26, 0xc6, 0xda)
-#define COGUI_HIGH_LIGHT        COGUI_RGB(0xf5, 0xf5, 0xf5)
-#define COGUI_LIGHT_GRAY        COGUI_RGB(0x9e, 0x9e, 0x9e)
-#define COGUI_DARK_GRAY         COGUI_RGB(0x42, 0x42, 0x42)
-#define COGUI_WHITE             COGUI_RGB(0xff, 0xff, 0xff)
-#define COGUI_BLACK             COGUI_RGB(0x00, 0x00, 0x00)
+/* some default color scheme */
+#define COGUI_RED               COGUI_RGB(0xc6, 0x28, 0x28)         /**< default red color          */ 
+#define COGUI_GREEN             COGUI_RGB(0x38, 0x8e, 0x3c)         /**< default green color        */ 
+#define COGUI_BLUE              COGUI_RGB(0x02, 0x77, 0xbd)         /**< default blue color         */ 
+#define COGUI_YELLOW            COGUI_RGB(0xfb, 0xc0, 0x2d)         /**< default yellow color       */ 
+#define COGUI_PURPLE            COGUI_RGB(0x51, 0x35, 0xb1)         /**< default purple color       */ 
+#define COGUI_CYAN              COGUI_RGB(0x26, 0xc6, 0xda)         /**< default cyan color         */ 
+#define COGUI_HIGH_LIGHT        COGUI_RGB(0xf5, 0xf5, 0xf5)         /**< default high light color   */ 
+#define COGUI_LIGHT_GRAY        COGUI_RGB(0x9e, 0x9e, 0x9e)         /**< default light gray color   */ 
+#define COGUI_DARK_GRAY         COGUI_RGB(0x42, 0x42, 0x42)         /**< default dark gray color    */ 
+#define COGUI_WHITE             COGUI_RGB(0xff, 0xff, 0xff)         /**< default white color        */ 
+#define COGUI_BLACK             COGUI_RGB(0x00, 0x00, 0x00)         /**< default black color        */ 
 
 #endif /* _COGUI_COLOR_H */
--- src/color.c
+++ src/color.c
@@ -9,5 +9,5 @@
 
 #include <cogui.h>
 
-const cogui_color_t default_foreground = COGUI_GREEN;
-const cogui_color_t default_background = COGUI_BLACK;
+const cogui_color_t default_foreground = COGUI_GREEN;       /**< set default foreground to green */
+const cogui_color_t default_background = COGUI_BLACK;       /**< set default background to black */
