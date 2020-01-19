4b7b0a0e3819f2fe88f7bfca5a5dda4a3f8c58fc
omuskywalker <610721230@gms.ndhu.edu.tw>
2020-01-19 17:56:14 +0800

    add comments for title.c

--- src/title.c
+++ src/title.c
@@ -1,16 +1,17 @@
 /**
  ********************************************************************************
  * @file       title.c
- * @version    V0.1.1
- * @date       2020.01.17
+ * @version    V0.1.2
+ * @date       2020.01.18
  * @brief      Title widget management function.
  *******************************************************************************
  */ 
 
 #include <cogui.h>
 
-const cogui_color_t default_title_background = COGUI_LIGHT_GRAY;
-const cogui_color_t default_title_foreground = COGUI_BLACK;
+const cogui_color_t default_title_background  = COGUI_BLACK;
+const cogui_color_t default_title_foreground  = COGUI_LIGHT_GRAY;
+const cogui_color_t default_button_foreground = COGUI_DARK_GRAY;
 
 extern cogui_font_t tm_symbol_16x16;
 
@@ -32,25 +33,28 @@ void cogui_title_create(cogui_window_t *win)
     cogui_widget_t *close_btn = cogui_widget_create(win);
     cogui_widget_t  *mini_btn = cogui_widget_create(win);
 
-    win->title->gc.foreground = COGUI_LIGHT_GRAY;
-    win->title->gc.background = COGUI_BLACK;
-    close_btn->gc.foreground = COGUI_DARK_GRAY;
-    close_btn->gc.background = COGUI_RED;
-    mini_btn->gc.foreground = COGUI_DARK_GRAY;
-    mini_btn->gc.background = COGUI_YELLOW;
+    /* filled default color scheme for title */
+    win->title->gc.foreground = default_title_foreground;
+    win->title->gc.background = default_title_background;
+    close_btn->gc.foreground  = default_button_foreground;
+    mini_btn->gc.foreground   = default_button_foreground;
 
+    /* enabled all three widgets */
     COGUI_WIDGET_ENABLE(win->title);
     COGUI_WIDGET_ENABLE(close_btn);
     COGUI_WIDGET_ENABLE(mini_btn);
 
+    /* give this three widgets flags */
     win->title->flag |= COGUI_WIDGET_FLAG_TITLE | COGUI_WIDGET_FLAG_RECT | COGUI_WIDGET_FLAG_FILLED;
-    close_btn->flag |= COGUI_WIDGET_FLAG_TITLE;
-    mini_btn->flag |= COGUI_WIDGET_FLAG_TITLE;
+    close_btn->flag  |= COGUI_WIDGET_FLAG_TITLE;
+    mini_btn->flag   |= COGUI_WIDGET_FLAG_TITLE;
 
+    /* set three widgets sizes */
     cogui_widget_set_rectangle(win->title, 65, 0, 240, COGUI_WINTITLE_HEIGHT);
     cogui_widget_set_rectangle(close_btn, 8, 0, 20, 40);
     cogui_widget_set_rectangle(mini_btn, 32, 0, 20, 40);
 
+    /* set title font style */
     cogui_widget_set_text_align(win->title, COGUI_TEXT_ALIGN_LEFT|COGUI_TEXT_ALIGN_MIDDLE);
     cogui_widget_set_font(win->title, &tm_font_16x26);
     cogui_widget_set_text(win->title, win->title_name);
@@ -67,12 +71,22 @@ void cogui_title_create(cogui_window_t *win)
     cogui_widget_set_text(mini_btn, "\"");
 }
 
+/**
+ *******************************************************************************
+ * @brief      Delete title widget set for window.
+ * @param[in]  *win    Which window this title below.
+ * @param[out] None
+ * @retval     None 
+ *******************************************************************************
+ */
 void cogui_title_delete(cogui_window_t *win)
 {
+    /* delete two button */
     cogui_widget_t *close_btn = win->title->next;
     cogui_widget_t  *mini_btn = win->title->next->next;
     cogui_widget_delete(close_btn);
     cogui_widget_delete(mini_btn);
 
+    /* title widget will delete at window deletion */
     COGUI_WIDGET_DISABLE(win->title);
 }
