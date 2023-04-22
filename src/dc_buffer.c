2669d74889df76d586cfad1c1de96720d3808edb
LuSkywalker <callumlu@gmail.com>
2023-04-22 23:13:18 +0800

    [M] empty function declare

--- src/dc_buffer.c
+++ src/dc_buffer.c
@@ -16,7 +16,7 @@ static void dc_buffer_draw_hline(dc_t *dc, S32 x1, S32 x2, S32 y);
 static void dc_buffer_fill_rect(dc_t *dc, rect_t rect);
 static StatusType dc_buffer_fini(dc_t *dc);
 
-const struct cogui_dc_engine dc_buffer_engine =
+const struct dc_engine dc_buffer_engine =
 {
     dc_buffer_draw_point,
     dc_buffer_draw_color_point,
@@ -27,3 +27,45 @@ const struct cogui_dc_engine dc_buffer_engine =
     dc_buffer_fini,
 };
 
+static void dc_buffer_draw_point(dc_t *dc, S32 x, S32 y)
+{
+    /* TODO: need implement
+     *  empty
+     */
+}
+
+static void dc_buffer_draw_color_point(dc_t *dc, S32 x, S32 y, color_t color)
+{
+    /* TODO: need implement
+     *  empty
+     */
+}
+
+static void dc_buffer_draw_vline(dc_t *dc, S32 x, S32 y1, S32 y2)
+{
+    /* TODO: need implement
+     *  empty
+     */
+}
+
+static void dc_buffer_draw_hline(dc_t *dc, S32 x1, S32 x2, S32 y)
+{
+    /* TODO: need implement
+     *  empty
+     */
+}
+
+static void dc_buffer_fill_rect(dc_t *dc, rect_t rect)
+{
+    /* TODO: need implement
+     *  empty
+     */
+}
+
+static StatusType dc_buffer_fini(dc_t *dc)
+{
+    /* TODO: need implement
+     *  empty
+     */
+    return E_OK;
+}
